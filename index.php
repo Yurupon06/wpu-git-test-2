3c87dc24fb5fe562d43ab3e1c1a57ed96f9bd983
iqbal <thesecretiqbal@gmail.com>
2024-07-04 11:47:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae rem nemo ratione quo ut assumenda voluptates delectus iure nisi architecto? Laudantium corporis deleniti cumque similique eligendi a dicta non sunt.</p>
+    <ul>
+        <li>sqtu</li>
+        <li>sqtu</li>
+        <li>sqtu</li>
+    </ul>
 </body>
 </html>
